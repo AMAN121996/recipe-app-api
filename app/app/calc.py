6015af2cc4df6e3280d1b3bb4c39dce2144c635c
Aman Ahirwar <amanahirwar74@gmail.com>
2025-05-08 19:01:21 +0530

    config database

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+
+Calculator functions
+
+"""
+
+def add(x, y ):
+    
+    return x + y
+
+def sub(x, y):
+
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+
+Sample tests
+
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_number(self):
+
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_number(self):
+        
+        res = calc.sub(10, 4)
+
+        self.assertEqual(res, 6)
