688b28ccbdf3971515b256fba17db5203a6c5ac6
Aman Ahirwar <amanahirwar74@gmail.com>
2025-05-09 12:02:22 +0530

    fixed linter error again

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,7 +5,11 @@ Calculator functions
 """
 
 def add(x, y):
+
+
     return x + y
 
 def sub(x, y):
+
+    
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,7 +5,9 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
 
     def test_add_number(self):
         res = calc.add(5, 6)
